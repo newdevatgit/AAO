9c917d6089876980bda80773117a2dca808a78b8
newdevatgit <arpanwar26@gmail.com>
2024-06-05 16:56:51 +0530

    1st commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.querySelector('.menu_icon img'); // Change this line
+  const navMenu = document.querySelector('.container3');
+
+  let isMenuOpen = false;
+
+  menuIcon.addEventListener('click', function () {
+    if (isMenuOpen) {
+      navMenu.style.display = 'none';
+      isMenuOpen = false;
+    } else {
+      navMenu.style.display = 'flex';
+      isMenuOpen = true;
+    }
+  });
+});
